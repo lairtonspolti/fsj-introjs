c8dbc6c53bc75998d0352ed7c2d6ab60996d887d
lairtonSpolti <spolti.lairton@gmail.com>
2020-09-20 12:52:24 -0300

    adicao das contants, e configuracao do server-dev, adicao global.css

---
+++ src/index.js
@@ -0,0 +1,5 @@
+import "./assets/css/global.css";
+
+import { API_ALBUM_URL } from "./contants";
+
+console.log(API_ALBUM_URL);
---
+++ webpack.config.js
@@ -0,0 +1,24 @@
+const path = require("path");
+
+module.exports = {
+  entry: "./src/index.js",
+  mode: "development",
+  output: {
+    filename: "main.js",
+    path: path.resolve(__dirname, "dist"),
+  },
+  devServer: {
+    contentBase: path.join(__dirname, "dist/"),
+    compress: true,
+    watchContentBase: true,
+    port: 9001,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
+    ],
+  },
+};
